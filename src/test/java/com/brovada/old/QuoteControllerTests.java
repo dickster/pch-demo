dbb74b2a876ad1cd9a448708d57a167a761dce6b
momogod <congchen15@gmail.com>
2017-09-25 16:42:48 -0400

    move old codes into another package

--- src/test/java/com/brovada/old/QuoteControllerTests.java
+++ src/test/java/com/brovada/old/QuoteControllerTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.brovada;
+package com.brovada.old;
 
 import com.brovada.old.Application;
 import org.junit.Before;
