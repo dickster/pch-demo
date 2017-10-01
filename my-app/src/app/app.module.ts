7c130d69b4008a75307affa5b64ace1a434d4d7a
dickster <derek.ipod@gmail.com>
2017-10-01 13:07:36 -0400

    start wih new my-app template.

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,14 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import {routes} from "./app.routes";
+import {LoginComponent} from "./login.component";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
