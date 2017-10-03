452058afcd9f9b1caae05940eddf75bbf7e91259
dickster <derek.ipod@gmail.com>
2017-10-03 13:41:14 -0400

    arggh...stupid AI page....dang frikkin' radio buttons....grumble.

--- my-app/src/app/ai.component.ts
+++ my-app/src/app/ai.component.ts
@@ -1,11 +1,18 @@
-import {Component, OnInit, ElementRef, Input} from '@angular/core';
+import {Component, NgModule, OnInit } from '@angular/core';
 
-import {FormControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from "@angular/router";
 import {Policy} from "./policy";
 import {Data} from "./data.store";
-import { DatePipe } from '@angular/common';
 
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+
+const Q1 = 'q1';
+const Q2 = 'q2';
+const Q1_DESC = 'q1_desc';
+const Q2_DESC = 'q2_desc';
 
 @Component({
     selector:'ai',
@@ -14,18 +21,34 @@ import { DatePipe } from '@angular/common';
 })
 export class AIComponent implements OnInit {
 
-    private policy:Policy;
+    public policy:Policy;
+    private form:FormGroup;
 
     constructor(
+        private formBuilder: FormBuilder,
         private data:Data<Policy>,
-        private router:Router,
-
+        private router:Router
        ) {
             this.policy = this.data.get();
+            this.form = this.formBuilder.group({
+                q1:[''],
+                q2:[''],
+                [Q1_DESC]:[],
+                [Q2_DESC]:[]
+            });
+            this.form.patchValue(this.policy);
     }
 
     ngOnInit() {
 
+    }
+
+    public isRadio(value:string) {
+        return 'true'==value;
+    }
+
+    public isNotRadio(value:string) {
+        return 'true'!=value;
     }
 
     handleSubmit(event:any) {
